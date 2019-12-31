e2f426fc6c39ab76b4962471c244caeacfdc9cfb
eron23 <eriicko23@yahoo.com>
2019-12-31 13:14:46 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,12 +37,6 @@ module.exports = {
                 pageContextProperty: `menus`,
                 menus: require('./src/data/menus.json'),
             }
-        },
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: "UA-155171672-1",
-            }
         }
     ]
 };
